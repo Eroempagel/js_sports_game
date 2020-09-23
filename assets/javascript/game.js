270849215b2732da56504cfea81ab19c604611a8
Erik Roempagel <eroempagel@outlook.com>
2020-09-23 16:41:22 -0500

    added audio to the code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,6 +10,46 @@
 // 2. Use what you know about HTML and CSS to make the page look better!
 // 3. Upon reset, display who won the game.
 
+// lets do sound?
+let myMusic;
+myMusic = new sound("assets/audio/practice.mp3");
+
+// may add a checkbox at top of page to play some background music?
+const checkbox = document.querySelector("#myCheck");
+checkbox.addEventListener("change", (event) => {
+  if (event.target.checked) {
+    console.log("Music checkbox is checked");
+    myMusic.play();
+  } else {
+    console.log("Music checkbox is not checked");
+    myMusic.stop();
+  }
+});
+
+// sound of arrow shooting
+let mySound1;
+mySound1 = new sound("assets/audio/arrow.mp3");
+
+// sound of arrow hitting
+let mySound2;
+mySound2 = new sound("assets/audio/arrow-wall.mp3");
+
+// sound handling function
+function sound(src) {
+  this.sound = document.createElement("audio");
+  this.sound.src = src;
+  this.sound.setAttribute("preload", "auto");
+  this.sound.setAttribute("controls", "none");
+  this.sound.style.display = "none";
+  document.body.appendChild(this.sound);
+  this.play = function () {
+    this.sound.play();
+  };
+  this.stop = function () {
+    this.sound.pause();
+  };
+}
+
 // Let's handle the reset button
 const resetContainer = document.querySelector("#reset-container");
 const resetBtn = document.querySelector("#reset-button");
@@ -32,11 +72,13 @@ resetBtn.addEventListener("click", function () {
 // then run a random number generator
 // if the random number is even that team scores a goal
 
+// Team One
 const teamOneNumshots = document.querySelector("#teamone-numshots");
 const teamOneShootButton = document.querySelector("#teamone-shoot-button");
 const teamOneGoals = document.querySelector("#teamone-numgoals");
 teamOneShootButton.addEventListener("click", function () {
   console.log("teamone's shoot button clicked");
+  mySound1.play();
   let newCounterValue = Number(teamOneNumshots.innerHTML) + 1;
   teamOneNumshots.innerHTML = newCounterValue;
   // returns a random integer
@@ -44,14 +86,17 @@ teamOneShootButton.addEventListener("click", function () {
   if (Math.floor(Math.random() * (10 - 0) + 1) % 2 == 0) {
     let newGoalValue = Number(teamOneGoals.innerHTML) + 1;
     teamOneGoals.innerHTML = newGoalValue;
+    mySound2.play();
   }
 });
 
+// Team Two
 const teamTwoNumshots = document.querySelector("#teamtwo-numshots");
 const teamTwoShootButton = document.querySelector("#teamtwo-shoot-button");
 const teamTwoGoals = document.querySelector("#teamtwo-numgoals");
 teamTwoShootButton.addEventListener("click", function () {
   console.log("teamtwo's shoot button clicked");
+  mySound1.play();
   let newCounterValue = Number(teamTwoNumshots.innerHTML) + 1;
   teamTwoNumshots.innerHTML = newCounterValue;
   // returns a random integer
@@ -59,5 +104,6 @@ teamTwoShootButton.addEventListener("click", function () {
   if (Math.floor(Math.random() * (10 - 0) + 1) % 2 == 0) {
     let newGoalValue = Number(teamTwoGoals.innerHTML) + 1;
     teamTwoGoals.innerHTML = newGoalValue;
+    mySound2.play();
   }
 });
